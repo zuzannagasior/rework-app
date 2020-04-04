cd001e07e54f2d3c99ef8a6c838944fc6da08e71
zuzannagasior <50328609+zuzannagasior@users.noreply.github.com>
2020-04-04 16:19:08 +0200

    employee profile modal

--- src/employer/EmployeeCard.js
+++ src/employer/EmployeeCard.js
@@ -1,53 +1,129 @@
-import React from 'react';
+import React from 'react'
+import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-export default function SimpleCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
 }
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    maxWidth: 800,
+    backgroundColor: theme.palette.background.paper,
+    // border: '2px solid #000',
+    // boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '50% !important',
+    left: '50% !important',
+    transform: 'translate(-50%, -50%) !important'
+  },
+}));
+
+const useStylesCard = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  
+  const EmployeeCard = (props) => {
+      const e = props.employee;
+      const classesCard = useStylesCard();
+      // const bull = <span className={classes.bullet}>•</span>;
+  
+      const classes = useStyles();
+      // getModalStyle is not a pure function, we roll the style only on the first render
+      const [modalStyle] = React.useState(getModalStyle);
+      const [open, setOpen] = React.useState(false);
+
+      const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+
+      const body = (
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">{e.name}</h2>
+          <p id="simple-modal-description">
+            <b>{e.job}</b> z {e.experience} letnim doświadczeniem
+            <br/>
+            <i>Władysławowo</i>
+          </p>
+          <p id="simple-modal-description">
+            Profil: 
+            <br />
+            {e.description}
+          </p>
+          {/* <EmployeeModal /> */}
+          <br />
+          <Button variant="contained" color="primary" size="medium">Nawiąż kontakt</Button>
+        </div>
+      );
+
+
+    return (
+      <Card className={classesCard.root}>
+        <CardContent>
+          <Typography className={classesCard.title} color="textSecondary" gutterBottom>
+            {e.category}
+          </Typography>
+          <Typography variant="h5" component="h2" className={classesCard.pos}>
+            {e.name}
+          </Typography>
+          {/* <Typography className={classesCard.pos} color="textSecondary">
+            adjective
+          </Typography> */}
+          {/* <Typography variant="body2" component="p">
+            Dostępność:
+            <br />
+            {bull} Od: {e.availabilityFrom}
+            <br />
+            {bull} Do: {e.availabilityFrom}
+          </Typography> */}
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary" size="small" onClick={handleOpen}>Zobacz profil</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            >
+            {body}
+        </Modal>
+        </CardActions>
+      </Card>
+    );
+  }
+
+  export default EmployeeCard;
--- src/employer/SearchEmployee.js
+++ src/employer/SearchEmployee.js
@@ -2,83 +2,41 @@ import React from 'react'
 import './Employer.css';
 import { NavLink } from 'react-router-dom';
 import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import EmployeeCard from './EmployeeCard.js'
 
 const employeeData = [
     {name: "Zuzia",
     category: "Gastronomia",
     availabilityFrom: "15.04.2020",
     availabilityTo: "19.04.2020",
-    },
+    job: "Kucharz",
+    experience: "5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    },
     {name: "Łukasz",
     category: "Branża hotelarska",
     availabilityFrom: "15.04.2020",
     availabilityTo: "19.04.2020",
+    job: "Kelner",
+    experience: "5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {name: "Madzia",
     category: "Branża eventowa",
     availabilityFrom: "15.04.2020",
     availabilityTo: "19.04.2020",
+    job: "Ochroniarz",
+    experience: "5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {name: "Radek",
     category: "Branża eventowa",
     availabilityFrom: "15.04.2020",
     availabilityTo: "19.04.2020",
+    job: "Muzyk",
+    experience: "5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 ];
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-const SimpleCard = (props) => {
-    const e = props.employee;
-    const classes = useStyles();
-    // const bull = <span className={classes.bullet}>•</span>;
-
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          {e.category}
-        </Typography>
-        <Typography variant="h5" component="h2" className={classes.pos}>
-          {e.name}
-        </Typography>
-        {/* <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography> */}
-        {/* <Typography variant="body2" component="p">
-          Dostępność:
-          <br />
-          {bull} Od: {e.availabilityFrom}
-          <br />
-          {bull} Do: {e.availabilityFrom}
-        </Typography> */}
-      </CardContent>
-      <CardActions>
-        <Button size="small">Zobacz profil</Button>
-      </CardActions>
-    </Card>
-  );
-}
 
 class SearchEmployee extends React.Component {
 
@@ -96,7 +54,7 @@ class SearchEmployee extends React.Component {
     render() {
 
         let employees = this.state.employeeDataToDisplay.map((e, index)=> (
-            <SimpleCard key={index} employee={e}/>
+            <EmployeeCard key={index} employee={e}/>
         ));
 
         return (
