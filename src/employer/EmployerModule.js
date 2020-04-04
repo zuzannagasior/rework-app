095ebe0ae75be9eaaa06e6c77b2586874768f673
zuzannagasior <50328609+zuzannagasior@users.noreply.github.com>
2020-04-04 13:08:12 +0200

    employer dashboard

--- src/employer/EmployerModule.js
+++ src/employer/EmployerModule.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Employer.css';
+import EmployerJobOfferList from './EmployerJobOfferList.js';
+import { NavLink } from 'react-router-dom';
 
 const EmployerModule = () => {
     return (
-        <div>
-            
+        <div class="em-container">
+            <nav>
+                <NavLink to="/search-employee"><button type="button">Szukaj pracownika</button></NavLink>
+                <NavLink to="/employer-module/add-job-offer"><button type="button">Dodaj ofertę pracy</button></NavLink>
+               
+                <EmployerJobOfferList />
+            </nav>
         </div>
     )
 }
--- src/employer/SearchEmployee.js
+++ src/employer/SearchEmployee.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchEmployee = () => {
     return (
         <div>
-            
+            Szukaj pracownika
         </div>
     )
 }
