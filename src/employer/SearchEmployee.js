38c0c1f702c06dd313b4ff46ed6ccdf5d7e5a640
zuzannagasior <50328609+zuzannagasior@users.noreply.github.com>
2020-04-04 14:23:45 +0200

    employee card in search employee

--- src/employer/SearchEmployee.js
+++ src/employer/SearchEmployee.js
@@ -2,6 +2,11 @@ import React from 'react'
 import './Employer.css';
 import { NavLink } from 'react-router-dom';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 const employeeData = [
     {name: "Zuzia",
@@ -26,6 +31,52 @@ const employeeData = [
     }
 ];
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const SimpleCard = () => {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
 class SearchEmployee extends React.Component {
 
     state={
@@ -36,7 +87,10 @@ class SearchEmployee extends React.Component {
     render() {
 
         let employees = this.state.employeeDataToDisplay.map((item, index)=> (
-            <p key={index}>{item.name}</p>
+            <div>
+                <p key={index}>{item.name}</p>
+                <SimpleCard />
+            </div>
         ));
 
         return (
